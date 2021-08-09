3c6ee2bbfe032b574c47cce4dc78f149f04ef20d
xujintao <xujintao@gmail.com>
2021-08-09 16:29:17 +0800

    js案例

---
+++ src/screen/projectList/SearchPanel.jsx
@@ -0,0 +1,25 @@
+export const SearchPanel = ({ users, param, setParam }) => {
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) => setParam({ ...param, name: evt.target.value })}
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) => setParam({ ...param, personId: evt.target.value })}
+        >
+          <option value="">全部</option>
+          {users &&
+            users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+        </select>
+      </div>
+    </form>
+  );
+};
